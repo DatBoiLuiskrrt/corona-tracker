27775c6852e303fee0d2aa1298ae04aa84827ea6
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-04-29 21:47:46 -0500

    Added the data to every country and switched from a chart to a bar for countries

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,19 +3,19 @@ import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 import styles from "./Chart.module.css";
 
-const Chart = () => {
+const Chart = ( { data: { confirmed, deaths, recovered}, country}) => {
     const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
-        console.log( dailyData);
+        // console.log( dailyData);
         fetchAPI();
-    });
+    },[]);
 
     const lineChart = (
-        // != 0
+        // != 0 this makes sures that if there is not data it won't display an error
         dailyData.length  
          ? (
             <Line
@@ -36,10 +36,34 @@ const Chart = () => {
         }}
         />) : null
     );
+    console.log(confirmed, recovered, deaths)
+    const barChart = (
+        confirmed 
+        ? (
+            <Bar
+            data={{
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [{
+                    label: 'People',
+                    backgroundColor: [
+                        "rgba(0,0,255,0.5)",
+                        "rgba(0,255,0,0.5)",
+                        "rgba(255,0,0,0.5)",
+                    ],
+                    data:[confirmed.value, recovered.value,deaths.value]
+                }]
+            }}
+            options={{
+                legend: { display: false},
+                title: { display: true, text: `Current state in ${country}`},
+            }}
+            />
+        ): null
+    )
 
     return (
        <div className={styles.container}>
-           {lineChart}
+           {country ? barChart : lineChart}
        </div>
     )
 }
